8e73491afb8384043a86540d217213bea724f6fe
Adrian K <adrian.git@kuguar.dev>
2018-03-26 16:22:19 -0300

    RealEstate index bare implementation.

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,142 +1,154 @@
-{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
-   buttons. Use this as a starting point for using elm-mdl components in your own
-   app.
--}
-
-
 module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (href, class, style)
-import Material
-import Material.Scheme
-import Material.Button as Button
-import Material.Options as Options exposing (css)
-import Material.Layout as Layout
+import Http
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline exposing (decode, required, optional)
 
 
--- MODEL
+-- import Debug exposing (log)
+
+
+type alias RealEstateResponse =
+    { list : List RealEstate
+    }
+
+
+type alias RealEstate =
+    { id : String
+    , name : String
+    , description : String
+    }
+
+
+realEstateSampleResponse : RealEstateResponse
+realEstateSampleResponse =
+    RealEstateResponse
+        [--RealEstate "1" "RealEstate1" "RealEstate1 description"
+         -- , RealEstate "2" "RealEstate2" "RealEstate2 description"
+        ]
+
+
+
+-- Model
 
 
 type alias Model =
-    { count : Int
-    , mdl :
-        Material.Model
-        -- Boilerplate: model store for any and all Mdl components you use.
+    { reSelectionId : String
+    , realEstateResponse : RealEstateResponse
+    , reListString : String
+    , error : String
     }
 
 
-model : Model
-model =
-    { count = 0
-    , mdl =
-        Material.model
-        -- Boilerplate: Always use this initial Mdl model store.
+initModel : Model
+initModel =
+    { reSelectionId = ""
+    , realEstateResponse = realEstateSampleResponse
+    , reListString = ""
+    , error = ""
     }
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, fetchRealEstateList )
 
--- ACTION, UPDATE
+
+decodeRealEstateResponse : Decoder RealEstateResponse
+decodeRealEstateResponse =
+  Json.Decode.map RealEstateResponse (list decodeRealEstate)
+       |> at [ "data" ]
+
+
+decodeRealEstate : Decoder RealEstate
+decodeRealEstate =
+    decode RealEstate
+        |> required "id" string
+        |> required "name" string
+        |> required "description" string
+
+
+fetchRealEstateList : Cmd Msg
+fetchRealEstateList =
+    let
+        url =
+            "/api/v1/real-estate"
+
+        request =
+            Http.get url decodeRealEstateResponse
+
+        cmd =
+            Http.send FetchRealEstateList request
+    in
+        cmd
+
+
+
+-- Update
 
 
 type Msg
-    = Increase
-    | Reset
-    | Mdl (Material.Msg Msg)
-
-
-
--- Boilerplate: Msg clause for internal Mdl messages.
+    = FetchRealEstateList (Result Http.Error RealEstateResponse)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increase ->
-            ( { model | count = model.count + 1 }
-            , Cmd.none
-            )
+        FetchRealEstateList (Ok result) ->
+            ( { model | realEstateResponse = result }, Cmd.none )
 
-        Reset ->
-            ( { model | count = 0 }
-            , Cmd.none
-            )
-
-        -- Boilerplate: Mdl action handler.
-        Mdl msg_ ->
-            Material.update Mdl msg_ model
+        FetchRealEstateList (Err err) ->
+            ( { model | error = toString (err) }, Cmd.none )
 
 
 
--- VIEW
-
-
-type alias Mdl =
-    Material.Model
+-- View
 
 
 view : Model -> Html Msg
 view model =
-    Layout.render Mdl
-        model.mdl
-        [ Layout.fixedHeader
-        ]
-        { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "Counter" ] ]
-        , drawer = []
-        , tabs = ( [], [] )
-        , main = [ viewBody model ]
-        }
-
-viewBody : Model -> Html Msg
-viewBody model =
     div
         [ style [ ( "padding", "2rem" ) ] ]
-        [ text ("Current count: " ++ toString model.count)
-          {- We construct the instances of the Button component that we need, one
-             for the increase button, one for the reset button. First, the increase
-             button. The first three arguments are:
-
-               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-               - An instance id (the `[0]`). Every component that uses the same model
-                 collection (model.mdl in this file) must have a distinct instance id.
-               - A reference to the elm-mdl model collection (`model.mdl`).
-
-             Notice that we do not have to add fields for the increase and reset buttons
-             separately to our model; and we did not have to add to our update messages
-             to handle their internal events.
-
-             Mdl components are configured with `Options`, similar to `Html.Attributes`.
-             The `Options.onClick Increase` option instructs the button to send the `Increase`
-             message when clicked. The `css ...` option adds CSS styling to the button.
-             See `Material.Options` for details on options.
-          -}
-        , Button.render Mdl
-            [ 0 ]
-            model.mdl
-            [ Options.onClick Increase
-            , css "margin" "0 24px"
+        [ div [] [ text "Real Estate List:" ]
+        , div []
+            [ renderRealEstateList model
             ]
-            [ text "Increase" ]
-        , Button.render Mdl
-            [ 1 ]
-            model.mdl
-            [ Options.onClick Reset ]
-            [ text "Reset" ]
         ]
-        |> Material.Scheme.top
+
+
+renderRealEstateList : Model -> Html Msg
+renderRealEstateList model =
+    model.realEstateResponse.list
+        |> List.map renderRealEstate
+        |> ul []
+
+
+renderRealEstate : RealEstate -> Html Msg
+renderRealEstate realEstate =
+    li []
+        [ text (realEstate.id ++ " - " ++ realEstate.name ++ " - " ++ realEstate.description)
+        ]
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
--- Load Google Mdl CSS. You'll likely want to do that not in code as we
--- do here, but rather in your master .html file. See the documentation
--- for the `Material` module for details.
+-- Init
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { init = ( model, Cmd.none )
-        , view = view
-        , subscriptions = always Sub.none
+        { init = init
         , update = update
+        , view = view
+        , subscriptions = subscriptions
         }
