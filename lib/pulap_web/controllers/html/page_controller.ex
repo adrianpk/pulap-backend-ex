b902b1eff0e4c13d4c7af3f64695bea732ac2b41
Adrian K <adrianpksw@gmail.com>
2018-05-10 22:17:23 -0300

    Authentication removed in Page controller.

--- lib/pulap_web/controllers/html/page_controller.ex
+++ lib/pulap_web/controllers/html/page_controller.ex
@@ -1,20 +1,8 @@
 defmodule PulapWeb.HTML.PageController do
-  use PulapWeb, :controller
 
-  plug :authenticate when action in [:index]
+  use PulapWeb, :controller
 
   def index(conn, _params) do
     render conn, "index.html"
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user() do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be signed in to access that page.")
-      |> redirect(to: session_path(conn, :new))
-      |> halt()
-    end
   end
 end
